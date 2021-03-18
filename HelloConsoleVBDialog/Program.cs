0972a73bd951bf528f2681a512a8ba6ef6ec6a47
krystianpetek <krystianpetek2@gmail.com>
2021-03-18 21:20:26 +0100

    commit poprawa

--- HelloConsoleVBDialog/Program.cs
+++ HelloConsoleVBDialog/Program.cs
@@ -13,18 +13,18 @@ namespace HelloConsoleVBDialog
         {
             Console.WriteLine("Program na powitanie.");
 
-            string imie = Interaction.InputBox("Podaj imię","Okienko imię");
+            string imie = Interaction.InputBox("Podaj imię", "Okienko imię");
             Console.WriteLine("Podano imię: " + imie);
 
             string nazwisko = Interaction.InputBox("Podaj nazwisko", "Okienko nazwisko");
-            Console.WriteLine("Podano nazwisko: "+nazwisko);
+            Console.WriteLine("Podano nazwisko: " + nazwisko);
 
             Interaction.MsgBox("Witaj " + imie + " " + nazwisko);
             Interaction.MsgBox(string.Format("Witaj {0} {1}. Czy Pan {0} rzeczywiście nazywa się {1}?", imie, nazwisko));
 
             int wiek = Convert.ToInt32(Interaction.InputBox("Podaj wiek", "Okienko wiek"));
             Console.WriteLine("Podano wiek: " + wiek);
-            if(wiek < 67)
+            if (wiek < 67)
             {
                 Interaction.MsgBox("do emerytury zostało Ci " + (67 - wiek) + "lat", MsgBoxStyle.OkOnly | MsgBoxStyle.Information, "Okienko emerytura");
             }
