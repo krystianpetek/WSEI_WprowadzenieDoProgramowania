f17368f9a8f52ffb1f95cb1dce36e767d822b3fe
krystianpetek <krystianpetek2@gmail.com>
2021-03-18 21:20:05 +0100

    update VB dialogbox

--- HelloConsoleVBDialog/Program.cs
+++ HelloConsoleVBDialog/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.VisualBasic;
 
 namespace HelloConsoleVBDialog
 {
@@ -10,7 +11,27 @@ namespace HelloConsoleVBDialog
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Program na powitanie.");
 
+            string imie = Interaction.InputBox("Podaj imię","Okienko imię");
+            Console.WriteLine("Podano imię: " + imie);
+
+            string nazwisko = Interaction.InputBox("Podaj nazwisko", "Okienko nazwisko");
+            Console.WriteLine("Podano nazwisko: "+nazwisko);
+
+            Interaction.MsgBox("Witaj " + imie + " " + nazwisko);
+            Interaction.MsgBox(string.Format("Witaj {0} {1}. Czy Pan {0} rzeczywiście nazywa się {1}?", imie, nazwisko));
+
+            int wiek = Convert.ToInt32(Interaction.InputBox("Podaj wiek", "Okienko wiek"));
+            Console.WriteLine("Podano wiek: " + wiek);
+            if(wiek < 67)
+            {
+                Interaction.MsgBox("do emerytury zostało Ci " + (67 - wiek) + "lat", MsgBoxStyle.OkOnly | MsgBoxStyle.Information, "Okienko emerytura");
+            }
+            else
+            {
+                Interaction.MsgBox("Jesteś emerytem", MsgBoxStyle.OkOnly | MsgBoxStyle.Information, "okienko emerytura");
+            }
         }
     }
 }
