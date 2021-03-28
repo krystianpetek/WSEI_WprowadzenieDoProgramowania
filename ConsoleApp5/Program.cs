21b6de91cf3567b778c3ad1b59e92fb574d6c97c
krystianpetek <krystianpetek2@gmail.com>
2021-03-28 19:48:01 +0200

    KC008 - Sumy wielokrotne dziala ale "zły wynik"
    dziala

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int suma3 = 0;
+            while (true)
+            {
+                var linia = Console.ReadLine();
+                if (linia == null || linia == "")
+                {
+                    break;
+                }
+                var tablica = linia.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                int suma = 0;
+                for (int i = 0; i < tablica.Length ;i++)
+                {
+                    
+                    suma += int.Parse(tablica[i]);
+                }
+                if (tablica.Length == 1 && int.Parse(tablica[0]) == 0)
+                {
+                    int sum = 0;
+                    Console.WriteLine(sum);
+                    break;
+                }
+
+                int suma2 = 0;
+                suma2 += suma;
+                Console.WriteLine(suma2);
+                suma3 += suma2;
+
+            }
+
+            Console.WriteLine(suma3);
+        }
+    }
+}
