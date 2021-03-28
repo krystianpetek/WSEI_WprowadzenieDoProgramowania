340e08fd2fce390e5b2593446a8285d67a1dcdac
krystianpetek <krystianpetek2@gmail.com>
2021-03-28 21:53:17 +0200

    Zadanie 9. SMPSUM - Iterated sums

--- ConsoleApp9/Program.cs
+++ ConsoleApp9/Program.cs
@@ -6,7 +6,17 @@ namespace ConsoleApp9
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var linia = Console.ReadLine();
+            var tab = linia.Split(" ",StringSplitOptions.RemoveEmptyEntries);
+            int a = int.Parse(tab[0]);
+            int b = int.Parse(tab[1]);
+            int suma = 0;
+            for (int i = a;i <= b ;i++)
+            {
+                int oblicz = i * i;
+                suma += oblicz;
+            }
+            Console.WriteLine(suma);
         }
     }
 }
