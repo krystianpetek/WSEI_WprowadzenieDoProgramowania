bc6277af27fce18dc7086df24ffb82aac31e38d7
krystianpetek <krystianpetek2@gmail.com>
2021-03-28 19:47:34 +0200

    Zadanie 4. RNO_DOD - Proste dodawanie

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int t = int.Parse(Console.ReadLine());
+            for (int i = 0; i < t; i++)
+            {
+                int n = int.Parse(Console.ReadLine());
+                var linia = Console.ReadLine();
+                var tablica = linia.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                int suma = 0;
+                for (int j = 0; j < n; j++)
+                {
+                    var x = int.Parse(tablica[j]);
+                    suma += x;
+                }
+                Console.WriteLine(suma);
+            }
+        }
+    }
+}
