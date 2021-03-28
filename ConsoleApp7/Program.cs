4fd26f2aaacde9706856e54baee5f6d31eb6ae11
krystianpetek <krystianpetek2@gmail.com>
2021-03-28 19:48:43 +0200

    first commit

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* while(true)
+             {
+                 var linia = Console.ReadLine();
+                 if (linia == null || linia == "") break;
+
+
+             }*/
+        }
+    }
+}
