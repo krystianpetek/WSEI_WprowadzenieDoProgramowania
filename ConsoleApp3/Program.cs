3137e4ba2e957215ade7762f94fa257a55ebb70a
krystianpetek <krystianpetek2@gmail.com>
2021-03-28 19:47:24 +0200

    Zadanie 3. SUMA - Suma

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int suma = 0;
+            while (true)
+            {
+                var linia = Console.ReadLine();
+                if (linia == null || linia == "")
+                {
+                    break;
+                }
+
+                var x = int.Parse(linia);
+                suma += x;
+                Console.WriteLine(suma);
+            }
+        }
+    }
+}
