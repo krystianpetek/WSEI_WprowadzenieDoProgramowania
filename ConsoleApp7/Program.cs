7c924a9ca24ee177a6a62683e009a52b29a1bc38
krystianpetek <krystianpetek2@gmail.com>
2021-03-28 20:20:19 +0200

    final

--- ConsoleApp7/Program.cs
+++ ConsoleApp7/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Numerics;
 
 namespace ConsoleApp7
 {
@@ -6,13 +7,20 @@ namespace ConsoleApp7
     {
         static void Main(string[] args)
         {
-            /* while(true)
-             {
-                 var linia = Console.ReadLine();
-                 if (linia == null || linia == "") break;
-
-
-             }*/
+            while (true)
+            {
+                BigInteger suma = 0;
+                var linia = Console.ReadLine();
+                if (linia == null || linia == "") break;
+                for (int i = 0; i < 2; i++)
+                {
+                    var tab = linia.Split(" ");
+                    BigInteger a = BigInteger.Parse(tab[0]);
+                    BigInteger b = BigInteger.Parse(tab[1]);
+                    suma = (a - b);
+                }
+                Console.WriteLine(suma);
+            }
         }
     }
 }
