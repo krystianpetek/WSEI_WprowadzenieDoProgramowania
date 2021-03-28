d3b11393a539b3a010c9f423d6362c4e1dc8a1aa
krystianpetek <krystianpetek2@gmail.com>
2021-03-28 19:47:09 +0200

    Zadanie 2. KC001 - Dodawanie liczb całkowitych

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int suma = 0;
+            
+            for(int i = 0; i < 3; i++)
+            {
+                var x = int.Parse(Console.ReadLine());
+                suma += x;
+            }
+            Console.WriteLine(suma);
+        }
+    }
+}
