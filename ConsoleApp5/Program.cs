b252d78e0b571522e4c557d42869190d6462faf5
krystianpetek <krystianpetek2@gmail.com>
2021-04-02 23:28:18 +0200

    ConsoleApp5_UPDATE SPOJ

--- ConsoleApp5/Program.cs
+++ ConsoleApp5/Program.cs
@@ -28,15 +28,35 @@ namespace ConsoleApp5
                     Console.WriteLine(sum);
                     break;
                 }
-
                 int suma2 = 0;
                 suma2 += suma;
                 Console.WriteLine(suma2);
                 suma3 += suma2;
-
             }
-
             Console.WriteLine(suma3);
+
+            //TO DZIALA NA SPOJ
+            suma3 = 0;
+            while (true)
+            {
+                var linia = Console.ReadLine();
+                if (linia == null || linia == "")
+                {
+                    break;
+                }
+                var tablica = linia.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                int suma = 0;
+                for (int i = 0; i < tablica.Length; i++)
+                {
+                    suma += int.Parse(tablica[i]);
+                }
+                int suma2 = 0;
+                suma2 += suma;
+                Console.WriteLine(suma2);
+                suma3 += suma2;
+            }
+            Console.Write(suma3);
         }
     }
 }
