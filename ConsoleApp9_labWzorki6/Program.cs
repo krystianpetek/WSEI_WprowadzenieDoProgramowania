2c0efb78db4e2f8de56923cb67f11b3b0afb1ff2
krystianpetek <krystianpetek2@gmail.com>
2021-04-04 01:38:50 +0200

    ConsoleApp9_labWzorki6

--- ConsoleApp9_labWzorki6/Program.cs
+++ ConsoleApp9_labWzorki6/Program.cs
@@ -13,9 +13,9 @@ namespace ConsoleApp9_labWzorki6
         static void NewLine() => Console.WriteLine();
         public static void X(int n)
         {
+            // GÓRNA CZĘŚĆ
             for (int i = 0; i < n/2; i++)
             {
-                
                 for (int j = 0; i> j; j++)
                     Space();
                 Star();
@@ -23,19 +23,21 @@ namespace ConsoleApp9_labWzorki6
                     Space();
                 StarNl();
             }
+
+            // JEŚLI NIEPARZYSTE
             if (n % 2 == 1)
                 for (int x = 0; x < n/2; x++)
                     Space();
                 StarNl();
+
+            // DOLNA CZĘŚĆ
             for(int i = n/2;i>0;i--)
             {
                 for(int j = i-1;j>0;j--)
                 Space();
                 Star();
                 for(int k = 0; k<n-2*i;k++)
-                {
                     Space();
-                }
                 StarNl();
             }
         }
