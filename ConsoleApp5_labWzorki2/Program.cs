be7dc4fe40c7acc92393dc74bb76839e5f355075
krystianpetek <krystianpetek2@gmail.com>
2021-04-03 18:24:28 +0200

    ConsoleApp5_labWzorki2

--- ConsoleApp5_labWzorki2/Program.cs
+++ ConsoleApp5_labWzorki2/Program.cs
@@ -13,50 +13,40 @@ namespace ConsoleApp5_labWzorki2
 
         public static void wyswietlX(int n)
         {
-            if (n < 3) throw new ArgumentException("zbyt mały rozmiar");
+            if (n < 3) throw new ArgumentException("Zbyt mały rozmiar");
             if (n % 2 == 0) n = n + 1;
 
-            for (int i = 0; i < n/2; i++)
+            // GÓRNA CZĘŚĆ
+            for (int i = 0; i < n / 2; i++)
             {
-                for(int j = 0; j < i;j++)
-                {
+                for (int j = 0; j < i; j++)
                     Space();
-                }
                 Star();
-                for(int j = 0;j<n -2 -2 *i;j++)
-                {
+                for (int j = 0; j < n - 2 - 2 * i; j++)
                     Space();
-                }
                 StarLn();
             }
 
-
-            for(int i = 0;i<n/2;i++)
-            {
+            // ŚRODKOWA LINIA
+            for (int i = 0; i < n / 2; i++)
                 Space();
-            }
             StarLn();
 
+            // DOLNA CZĘŚĆ
             int m = n;
-
-            for(int x = m/2; x> 0; x--) 
+            for (int x = m / 2; x > 0; x--)
             {
-                for(int i = x-1; i>0 ;i--)
-                {
+                for (int i = x - 1; i > 0; i--)
                     Space();
-                }
                 Star();
-                for(int j=0;j<m-2*x;j++)
-                {
+                for (int j = 0; j < m - 2 * x; j++)
                     Space();
-                }
                 StarLn();
-
             }
         }
         static void Main(string[] args)
         {
-            wyswietlX(14);
+            wyswietlX(11);
         }
     }
 }
