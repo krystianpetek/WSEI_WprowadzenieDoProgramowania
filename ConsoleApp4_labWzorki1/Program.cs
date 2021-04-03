50e4fa16e4fd66fa35b456b84485546381527955
krystianpetek <krystianpetek2@gmail.com>
2021-04-03 14:23:57 +0200

    ConsoleApp4_labWzorki1

---
+++ ConsoleApp4_labWzorki1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ConsoleApp4_labWzorki1
+{
+    class Program
+    {
+        // labWzorki1
+        const char CHAR = '*';
+        static void Star() => Console.Write(CHAR);
+        static void StarLn() => Console.WriteLine(CHAR);
+        static void Space() => Console.Write(' ');
+        static void SpaceLn() => Console.WriteLine(' ');
+        static void NewLine() => Console.WriteLine();
+
+        public static void Prostokat(int n, int m)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                Star();
+            }
+            NewLine();
+
+            for(int j = 1; j<m-1;j++)
+            {
+                Star();
+                for(int i = 1; i< n-1;i++)
+                {
+                    Space();
+                }
+                StarLn();
+            }
+            for (int i = 0; i < n; i++)
+            {
+                Star();
+            }
+            NewLine();
+        }
+        public static void Main(string[] args)
+        {
+            Prostokat(7,5);
+        }
+    }
+}
