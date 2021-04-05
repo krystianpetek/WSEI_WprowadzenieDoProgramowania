72cc20bf5cd63c4e679138fa73a0779863f6f71c
krystianpetek <krystianpetek2@gmail.com>
2021-04-05 16:48:16 +0200

    ConsoleApp2_funWithSequences2

---
+++ ConsoleApp2_funWithSequences2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApp2_funWithSequences2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
