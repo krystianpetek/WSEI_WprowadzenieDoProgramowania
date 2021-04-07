07fd566277ae7acf1bb5348d630500da9beedcde
krystianpetek <krystianpetek2@gmail.com>
2021-04-07 22:26:36 +0200

    ConsoleApp5_funWithSequences5

--- ConsoleApp5_funWithSequences5/Program.cs
+++ ConsoleApp5_funWithSequences5/Program.cs
@@ -16,34 +16,31 @@ namespace ConsoleApp5_funWithSequences5
             int n = int.Parse(Console.ReadLine());
 
             if (n < 2 || n > 100)
-            {
                 throw new ArgumentException("Liczba z poza zakresu 2-100");
-            }
             string S = Console.ReadLine();
             string[] tablicaS = S.Split(" ", StringSplitOptions.RemoveEmptyEntries);
 
             if (tablicaS.Length != n)
                 throw new ArgumentException("Ilosc liczb w zbiorze rózna od deklaracji ilosci liczb");
 
-            int dlugoscS = n-1;
+            int dlugoscS = n - 1;
             int x = 0;
-
 
             for (; x < tablicaS.Length; x++)
                 if (Convert.ToInt32(tablicaS[x]) > Convert.ToInt32(tablicaS[x + 1]))
                     dlugoscS--;
-                else {
+                else
+                {
                     x++;
                     break;
                 }
-            for (int y = x; y < tablicaS.Length-1; y++) 
-            {
+            
+            for (int y = x; y < tablicaS.Length - 1; y++)
                 if (Convert.ToInt32(tablicaS[y]) < Convert.ToInt32(tablicaS[y + 1]))
                     dlugoscS--;
                 else
                     break;
-            }
-            if(dlugoscS==1)
+            if (dlugoscS == 1)
                 Console.WriteLine("Yes");
             else
                 Console.WriteLine("No");
