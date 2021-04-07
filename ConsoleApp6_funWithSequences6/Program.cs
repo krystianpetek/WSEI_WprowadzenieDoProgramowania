ac3b43636d6ad74ffb6c4404f0618590f2a1403d
krystianpetek <krystianpetek2@gmail.com>
2021-04-07 22:43:04 +0200

    ConsoleApp6_funWithSequences6

---
+++ ConsoleApp6_funWithSequences6/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ConsoleApp6_funWithSequences6
+{
+    class Program
+    {
+        // Fun With Sequences Act6
+
+        /* DANE WEJSCIOWE
+         * 5
+         * -2 -1 0 1 4
+         * 6
+         * -3 -2 -1 1 2 3
+         */
+
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string S = Console.ReadLine();
+            string[] tablicaS = S.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            if (tablicaS.Length != n)
+                throw new ArgumentException("Error");
+            
+            int m = int.Parse(Console.ReadLine());
+            string Q = Console.ReadLine();
+            string[] tablicaQ = Q.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            if (tablicaQ.Length != m)
+                throw new ArgumentException("Error");
+
+            int sumaS = 0;
+            for (int x = 0; x < n; x++)
+                 sumaS += int.Parse(tablicaS[x]);
+
+            int sumaQ = 0;
+            for (int y = 0; y < m; y++)
+                sumaQ += int.Parse(tablicaQ[y]);
+
+            if(sumaS > sumaQ)
+                Console.WriteLine(S);
+            else
+                Console.WriteLine(Q);
+        }
+    }
+}
