452d05a94219c87a7f57c551226396de33889a70
krystianpetek <krystianpetek2@gmail.com>
2021-04-09 22:52:25 +0200

    ConsoleApp10_characterPatterns1

---
+++ ConsoleApp10_characterPatterns1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ConsoleApp10_characterPatterns1
+{
+    class Program
+    {
+        // Character Patterns 1
+        // DANE WEJSCIOWE:
+        // 3
+        // 3 1
+        // 4 4
+        // 2 5
+
+        static void Main(string[] args)
+        {
+            int t = int.Parse(Console.ReadLine());
+            for (int i = 0; i < t; i++)
+            {
+                string linia = Console.ReadLine();
+                string[] ciagLiczb = linia.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                int l = int.Parse(ciagLiczb[0]);
+                int c = int.Parse(ciagLiczb[1]);
+                for (int y = 0; y < l; y++)
+                {
+                    for (int x = 0; x < c; x++)
+                    {
+                        if (y % 2 == 0)
+                        {
+                            if (x % 2 == 1)
+                                Console.Write(".");
+                            else
+                                Console.Write("*");
+                        }
+                        else
+                        {
+                            if (x % 2 == 0)
+                                Console.Write(".");
+                            else
+                                Console.Write("*");
+                        }
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
