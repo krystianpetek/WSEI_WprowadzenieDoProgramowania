2630102edcf3390ca3cd20445417c304982fb808
krystianpetek <krystianpetek2@gmail.com>
2021-04-18 13:40:46 +0200

    commit first

---
+++ ConsoleApp1_rownanieKwadratowe/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApp1_rownanieKwadratowe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
