f9194cbecc4b54618324e224e088e8868c94978d
krystianpetek <krystianpetek2@gmail.com>
2021-04-19 20:40:29 +0200

    ConsoleApp14_characterPatterns5

--- ConsoleApp14_characterPatterns5/Program.cs
+++ ConsoleApp14_characterPatterns5/Program.cs
@@ -10,6 +10,7 @@ namespace ConsoleApp14_characterPatterns5
         // 3 1 2
         // 4 4 1
         // 2 5 2
+
         static void Main(string[] args)
         {
             int t = int.Parse(Console.ReadLine());
@@ -17,8 +18,8 @@ namespace ConsoleApp14_characterPatterns5
             {
                 string linia = Console.ReadLine();
                 string[] ciagLiczb = linia.Split(" ", StringSplitOptions.RemoveEmptyEntries);
-                int l = int.Parse(ciagLiczb[0]); // Wiersz
-                int c = int.Parse(ciagLiczb[1]); // Kolumna
+                int l = int.Parse(ciagLiczb[0]); // WIERSZ
+                int c = int.Parse(ciagLiczb[1]); // KOLUMNA
                 int s = int.Parse(ciagLiczb[2]); // KWADRAT
 
                 for (int iloscWierszy = 0; iloscWierszy < l; iloscWierszy++)
@@ -35,125 +36,67 @@ namespace ConsoleApp14_characterPatterns5
                             Console.WriteLine("*");
                     }
 
-
-
-
-
-
-
                     for (int liniaWysokosc = 0; liniaWysokosc < s; liniaWysokosc++)
                     {
-
-                        for (int gwiazdkiLinia = 0; gwiazdkiLinia < c; gwiazdkiLinia++)
+                        for (int kolumna = 0; kolumna < c; kolumna++)
                         {
-                            int liniaSzerokosc;
                             Console.Write("*");
-                            for (liniaSzerokosc = 0; liniaSzerokosc < s; liniaSzerokosc++)
+                            if (iloscWierszy % 2 == 0)
                             {
-
-                                if (iloscWierszy % 2 == 0)
+                                if (kolumna % 2 == 1)
                                 {
-                                    if (liniaWysokosc % 2 == 0)
+                                    for (int x = s; x > 0; x--)
                                     {
-
-                                        if (gwiazdkiLinia % 2 == 0)
-                                        {
-
-                                            if (liniaSzerokosc % 2 == 0)
-                                                Console.Write(@"\");
-                                            else
-                                                Console.Write(".");
-                                        }
+                                        if (liniaWysokosc == x - 1)
+                                            Console.Write(@"/");
                                         else
-                                        {
-                                            if (liniaSzerokosc % 2 == 0 && s > 1)
-                                                Console.Write(".");
-                                            else
-                                                Console.Write(@"/");
-                                        }
-                                    }
-                                    else
-                                    {
-                                        if (gwiazdkiLinia % 2 == 0)
-                                        {
-
-                                            if (liniaSzerokosc % 2 == 0 && s > 1)
-                                                Console.Write(@".");
-                                            else
-                                                Console.Write(@"\");
-                                        }
-                                        else
-                                        {
-                                            if (liniaSzerokosc % 2 == 0)
-                                                Console.Write(@"/");
-                                            else
-                                                Console.Write(".");
-                                        }
+                                            Console.Write(".");
                                     }
                                 }
                                 else
                                 {
-                                    if (liniaWysokosc % 2 == 0)
+                                    for (int x = 0; x < s; x++)
                                     {
-
-                                        if (gwiazdkiLinia % 2 == 0)
-                                        {
-
-                                            if (liniaSzerokosc % 2 == 0 && s > 1)
-
-                                                Console.Write(".");
-                                            else
-                                                Console.Write(@"/");
-                                        }
+                                        if (liniaWysokosc == x)
+                                            Console.Write(@"\");
                                         else
-                                        {
-                                            if (liniaSzerokosc % 2 == 0)
-                                                Console.Write(@"\");
-                                            else
-                                                Console.Write(".");
-                                        }
-                                    }
-                                    else
-                                    {
-                                        if (gwiazdkiLinia % 2 == 0)
-                                        {
-
-                                            if (liniaSzerokosc % 2 == 0)
-                                                Console.Write(@"/");
-                                            else
-                                                Console.Write(@".");
-                                        }
-                                        else
-                                        {
-                                            if (liniaSzerokosc % 2 == 0 && s > 1)
-                                                Console.Write(".");
-                                            else
-                                                Console.Write(@"\");
-                                        }
+                                            Console.Write(".");
                                     }
                                 }
-
                             }
+                            else
+                            {
+                                if (kolumna % 2 == 0)
+                                {
 
-                            if (gwiazdkiLinia == c - 1)
-                                Console.WriteLine("*");
+                                    for (int x = s; x > 0; x--)
+                                    {
+                                        if (liniaWysokosc == x - 1)
+                                            Console.Write(@"/");
+                                        else
+                                            Console.Write(".");
+                                    }
+                                }
+                                else
+                                {
+                                    for (int x = 0; x < s; x++)
+                                    {
+                                        if (liniaWysokosc == x)
+                                            Console.Write(@"\");
+                                        else
+                                            Console.Write(".");
+                                    }
+                                }
+                            }
                         }
-
-
-
+                        Console.WriteLine("*");
                     }
-
-
-
-
-
 
                     if (iloscWierszy == l - 1)
                     {
                         int gwiazdkiOstatnie;
                         for (int ostatniaLinia = 0; ostatniaLinia < c; ostatniaLinia++)
                         {
-
                             Console.Write("*");
                             for (gwiazdkiOstatnie = 0; gwiazdkiOstatnie < s; gwiazdkiOstatnie++)
                                 Console.Write("*");
@@ -163,124 +106,7 @@ namespace ConsoleApp14_characterPatterns5
                         }
                     }
                 }
-
-
-
                 Console.WriteLine();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-                /*int x, // c
-                y, // l
-                A = 0;
-
-                int PRAWO;
-
-                for (y = 0; y < l; y++)
-                {
-                    //PIERWSZA LINIA
-                    for (x = 0; x < c; x++)
-                    {
-                        Console.Write("*");
-                        for (PRAWO = 0; PRAWO < s; PRAWO++)
-                            Console.Write("*");
-
-                        if (x == c - 1)
-                            Console.WriteLine("*");
-                    }
-
-
-                    for (int DOL = 0; DOL < s; DOL++)
-                    {
-                        for (x = 0; x < c; x++)
-                        {
-                            Console.Write("*");
-                            for (PRAWO = 0; PRAWO < s; PRAWO++)
-                            {
-                                if (DOL % 2 == 1)
-                                {
-                                    if (x % 2 == 0)
-                                    {
-                                        if (PRAWO % 2 == 0)
-                                            Console.Write(".");
-                                        else
-                                            Console.Write(@"\");
-                                    }
-                                    else
-                                    {
-                                        if (PRAWO % 2 == 0)
-                                            Console.Write(@"/");
-                                        else
-                                            Console.Write(".");
-                                    }
-                                }
-                                else
-                                {
-                                    if (x % 2 == 0)
-                                    {
-                                        if (PRAWO % 2 == 0)
-                                            Console.Write(@"\");
-                                        else
-                                            Console.Write(".");
-                                    }
-                                    else
-                                    {
-                                        if (PRAWO % 2 == 0)
-                                            Console.Write(".");
-                                        else
-                                            Console.Write(@"/");
-                                    }
-                                }
-                            }
-                            if (x == c - 1)
-                                Console.WriteLine("*");
-
-                        }
-                    }
-
-                    if(y == l-1)
-                    {
-                        for (x = 0; x < c; x++)
-                        {
-                            Console.Write("*");
-                            for (PRAWO = 0; PRAWO < s; PRAWO++)
-                                Console.Write("*");
-
-                            if (x == c - 1)
-                                Console.WriteLine("*");
-                        }
-                    }
-                }
-                */
             }
         }
     }
