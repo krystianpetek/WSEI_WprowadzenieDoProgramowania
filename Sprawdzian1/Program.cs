6da9bee7672f7f693acd432daf9d58054f5c3ae9
krystianpetek <krystianpetek2@gmail.com>
2021-05-03 22:04:33 +0200

    SPRAWDZIAN

---
+++ Sprawdzian1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Sprawdzian1
+{
+    class Program
+    {
+        public static void Wzorek(int n)
+        {
+           
+            int wejscie = n/2; 
+            if (n % 2 == 0)
+                wejscie -= 1;
+            
+            for (int i = 0; i < wejscie+1; i++)
+            {
+                for (int j = 0; j < i; j++)
+                    Console.Write(" ");
+                
+                for (int x = wejscie+1; x > i; x--)
+                    Console.Write("*");
+                
+                for (int c = wejscie; c > i; c--)
+                    Console.Write("*");
+                Console.WriteLine();
+            }
+        }
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Wzorek(n);
+        }
+    }
+}
