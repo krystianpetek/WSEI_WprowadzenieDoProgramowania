cd5b0b1a102da4c61c9c7f74a5f7c030906ffdaa
krystianpetek <krystianpetek2@gmail.com>
2021-05-04 23:56:59 +0200

    ConsoleApp4_hotelFloors

--- ConsoleApp4_hotelFloors/Program.cs
+++ ConsoleApp4_hotelFloors/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
-//3
+
+//4
 //5 5
 //#####
 //#**##
@@ -23,9 +24,10 @@ using System.Collections;
 //5 14
 //##############
 //#----###-*-#-#
-//###***##*#**-#
+//###***##*#-*-#
 //#***-#########
 //##############
+
 namespace ConsoleApp4_hotelFloors
 {
     class Program
@@ -40,7 +42,7 @@ namespace ConsoleApp4_hotelFloors
                 int M = Convert.ToInt32(linia[0]);
                 int N = Convert.ToInt32(linia[1]);
 
-                // PRZYPISZ WARTOSCI DO TABLICY
+                // PRZYPISANIE WARTOŚCI DO TABLICY
                 char[,] rzutGora = new char[M, N];
                 for (int x = 0; x < rzutGora.GetLength(0); x++)
                 {
@@ -54,7 +56,7 @@ namespace ConsoleApp4_hotelFloors
                     }
                 }
 
-                // LICZENIE OSÓB
+                // LICZENIE OSOB
                 int liczbaOsob = 0;
                 for (int aa = 1; aa < rzutGora.GetLength(0) - 1; aa++)
                     for (int bb = 1; bb < rzutGora.GetLength(1) - 1; bb++)
@@ -63,93 +65,90 @@ namespace ConsoleApp4_hotelFloors
 
                 // LICZENIE POMIESZCZEŃ
                 int pomieszczenie = 0;
-                int a = 1;
-                int b = 1;
-                int licznik = 0;
-                Stack myStack = new Stack();
+                Stack stosPrzebiegu = new Stack();
 
-                //for (int z = 1; z < rzutGora.GetLength(0) - 1; z++)
-                //{
-                //    for (int x = 1; x < rzutGora.GetLength(1) - 1; x++)
-                //    {
-                //        if (rzutGora[z, x] == '*' || rzutGora[z, x] == '-')
-                //        {
-                while (b < rzutGora.GetLength(1))
+                for (int a = 1; a < rzutGora.GetLength(0) - 1; a++)
                 {
-                    var gora = rzutGora[a - 1, b];
-                    var prawo = rzutGora[a, b + 1];
-                    var dol = rzutGora[a + 1, b];
-                    var lewo = rzutGora[a, b - 1];
-
-                    Console.WriteLine($"{a},{b}, {rzutGora[a, b]} COUNT{myStack.Count} {gora},{prawo},{dol},{lewo}");
-                    rzutGora[a, b] = '.';
-
-
-                    if (gora == '#' | gora == '.' && prawo == '#' | prawo == '.' && dol == '#' | dol == '.' && lewo == '#' | lewo == '.')
+                    for (int b = 1; b < rzutGora.GetLength(1) - 1; b++)
                     {
-                        licznik++;
-                        Console.WriteLine("ile razy: " + licznik);
-                        if (myStack.Count > 0)
-                        {
-                            b = Convert.ToInt32(myStack.Pop());
-                            a = Convert.ToInt32(myStack.Pop());
-                        }
+                        if (rzutGora[a, b] == '.' || rzutGora[a, b] == '#')
+                            continue;
                         else
                         {
-                            pomieszczenie++;
-                            break;
+                            while (true)
+                            {
+                                var gora = rzutGora[a - 1, b];
+                                var prawo = rzutGora[a, b + 1];
+                                var dol = rzutGora[a + 1, b];
+                                var lewo = rzutGora[a, b - 1];
+
+                                //Console.WriteLine($"{a},{b}, {rzutGora[a, b]} COUNT{myStack.Count} {gora},{prawo},{dol},{lewo}");
+                                rzutGora[a, b] = '.';
+
+                                if (gora == '#' | gora == '.' && prawo == '#' | prawo == '.' && dol == '#' | dol == '.' && lewo == '#' | lewo == '.')
+                                {
+                                    if (stosPrzebiegu.Count > 0)
+                                    {
+                                        //Console.WriteLine($"{a},{b}, {rzutGora[a, b]} COUNT{myStack.Count} {gora},{prawo},{dol},{lewo}");
+                                        b = Convert.ToInt32(stosPrzebiegu.Pop());
+                                        a = Convert.ToInt32(stosPrzebiegu.Pop());
+                                        continue;
+                                    }
+                                    else
+                                    {
+                                        pomieszczenie++;
+                                        break;
+                                    }
+
+                                }
+                                if (gora == '-' || gora == '*')
+                                {
+                                    stosPrzebiegu.Push(a);
+                                    stosPrzebiegu.Push(b);
+                                    a--;
+                                    continue;
+                                }
+
+                                if (prawo == '-' || prawo == '*')
+                                {
+                                    stosPrzebiegu.Push(a);
+                                    stosPrzebiegu.Push(b);
+                                    b++;
+                                    continue;
+                                }
+
+                                if (dol == '-' || dol == '*')
+                                {
+                                    stosPrzebiegu.Push(a);
+                                    stosPrzebiegu.Push(b);
+                                    a++;
+                                    continue;
+                                }
+
+                                if (lewo == '-' || lewo == '*')
+                                {
+                                    stosPrzebiegu.Push(a);
+                                    stosPrzebiegu.Push(b);
+                                    b--;
+                                    continue;
+                                }
+                            }
                         }
-
                     }
-                    if (gora == '-' || gora == '*')
-                    {
-                        myStack.Push(a);
-                        myStack.Push(b);
-                        a--;
-                        continue;
-                    }
-
-                    if (prawo == '-' || prawo == '*')
-                    {
-                        b++;
-                        myStack.Push(a);
-                        myStack.Push(b);
-                        continue;
-                    }
-
-                    if (dol == '-' || dol == '*')
-                    {
-                        myStack.Push(a);
-                        myStack.Push(b);
-                        a++;
-                        continue;
-                    }
-
-                    if (lewo == '-' || lewo == '*')
-                    {
-                        myStack.Push(a);
-                        myStack.Push(b);
-                        b--;
-                        continue;
-                    }
-
                 }
-                //        }
-                //    }
-                //}
 
-                // OBLICZENIE ZAGĘSZCZENIA PIĘTRA
+                // OBLICZENIE WYNIKU
                 double oblicz = (double)liczbaOsob / (double)pomieszczenie;
                 Console.WriteLine($"{Math.Round(oblicz, 2):F2}");
 
-                // WYPISZ PRZELICZONY WIDOK PIĘTRA
-                for (int iter = 0; iter < M; iter++)
-                    for (int iter2 = 0; iter2 < N; iter2++)
-                    {
-                        Console.Write(rzutGora[iter, iter2]);
-                        if (iter2 == N - 1)
-                            Console.WriteLine();
-                    }
+                // WYSWIETL PRZELICZONE PIETRO
+                //for (int iter = 0; iter < M; iter++)
+                //    for (int iter2 = 0; iter2 < N; iter2++)
+                //    {
+                //        Console.Write(rzutGora[iter, iter2]);
+                //        if (iter2 == N - 1)
+                //            Console.WriteLine();
+                //    }
             }
         }
     }
