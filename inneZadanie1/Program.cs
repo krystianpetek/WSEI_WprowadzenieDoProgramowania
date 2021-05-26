70667537e3f1df73646446d7a068ae93e8ee6000
krystianpetek <krystianpetek2@gmail.com>
2021-05-26 19:33:31 +0200

    inneZadanie1

---
+++ inneZadanie1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace inneZadanie1
+{
+    class Program
+    {
+        public static void Wzorek(int n)
+        {
+            for (int pierwsza = 0; pierwsza < n - 1; pierwsza++)
+                Console.Write("*");
+            Console.WriteLine();
+
+            for (int srodkowa = (n - 3) / 2; srodkowa > 0; srodkowa--)
+            {
+                for (int pierwsza = 0; pierwsza < n - 1; pierwsza++)
+                    Console.Write(" ");
+
+                Console.WriteLine("*");
+            }
+            for (int pierwsza = 0; pierwsza < n - 1; pierwsza++)
+                Console.Write("*");
+            Console.WriteLine();
+
+            for (int srodkowa = (n - 3) / 2; srodkowa > 0; srodkowa--)
+            {
+                for (int pierwsza = 0; pierwsza < n - 1; pierwsza++)
+                    Console.Write(" ");
+
+                Console.WriteLine("*");
+            }
+
+            for (int pierwsza = 0; pierwsza < n - 1; pierwsza++)
+                Console.Write("*");
+        }
+
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            if (n < 5)
+                throw new ArgumentException("Błędne dane n musi być większe od 5");
+            Wzorek(n);
+        }
+    }
+}
