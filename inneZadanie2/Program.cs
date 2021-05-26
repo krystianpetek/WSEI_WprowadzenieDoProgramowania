82698ae89c7748abfe460d401e04a99b150d3e49
krystianpetek <krystianpetek2@gmail.com>
2021-05-26 19:34:36 +0200

    inneZadanie2

---
+++ inneZadanie2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace inneZadanie2
+{
+    class Program
+    {
+        static void Wzorki(int n)
+        {
+
+            for (int i = 0; i < n; i++)
+                Console.Write("*");
+            Console.WriteLine();
+            for (int x = n; x > 1; x--)
+            {
+                for (int y = 2; y < x; y++)
+                    Console.Write(" ");
+                Console.WriteLine("*");
+            }
+        }
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Wzorki(n);
+        }
+    }
+}
