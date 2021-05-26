448a5ab616b2e4f12394423a0e7be3ce818fc7a0
krystianpetek <krystianpetek2@gmail.com>
2021-05-26 23:33:28 +0200

    sprawdzianZadanie4

--- sprawdzianZadanie4/Program.cs
+++ sprawdzianZadanie4/Program.cs
@@ -1,40 +1,59 @@
 ﻿using System;
-
+using System.Text;
 namespace sprawdzianZadanie4
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-
         public static void Print(int[] a, int[] b)
         {
-            int i = 0;
-            int j = 0;
-            string wynik = "";
-            for (; i < a.Length; i++)
+            StringBuilder wynik = new StringBuilder();
+            for (int x = 0; x < a.Length; x++)
             {
-                for (; j < b.Length; j++)
+                bool check = true;
+                for (int y = 0; y < b.Length; y++)
                 {
-
+                    if (a[x] == b[y])
+                        check = false;
                 }
+                if (check == true)
+                    wynik.Append($"{a[x]} ");
+                    
             }
-            Console.WriteLine(wynik);
+            
+            for (int x = 0; x < b.Length; x++)
+            {
+                bool check = true;
+                for (int y = 0; y < a.Length; y++)
+                {
+                    if (b[x] == a[y])
+                        check = false;
+                }
+                if (check == true)
+                    wynik.Append($"{b[x]} ");
+                    
+            }
+
+            if (wynik.ToString() == "")
+            {
+                Console.WriteLine("empty");
+                return;
+            }
+            string[] tab = wynik.ToString().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            Array.Sort(tab);
+            foreach(var x in tab)
+                Console.Write($"{x} ");
         }
         static void Main()
         {
-            int[] a = new int[] { 0, 1, 1, 2, 3, 3, 3 };
-            int[] b = new int[] { 0, 1, 2, 3, 3 };
-            //int[] a = new int[] { -2, -1, 0, 1, 4 };
-            //int[] b = new int[] { -3, -2, -1, 1, 2, 3 };
+            //int[] a = new int[] { 0, 1, 1, 2, 3, 3, 3 };
+            //int[] b = new int[] { 0, 1, 2, 3, 3 };
+            int[] a = new int[] { -2, -1, 0, 1, 4 };
+            int[] b = new int[] { -3, -2, -1, 1, 2, 3 };
 
             //int[] a = new int[] { -2, -1, 0, 1, 4 };
             //int[] b = new int[] { -2, -1, 0, 1, 4, 5, 6 };
             Print(a, b);
         }
-
     }
 
 }
