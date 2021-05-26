10d9dae92dc80124fadcbd8afecf52bd3fa37ad5
krystianpetek <krystianpetek2@gmail.com>
2021-05-26 19:35:23 +0200

    WIP

--- sprawdzianZadanie4/Program.cs
+++ sprawdzianZadanie4/Program.cs
@@ -8,5 +8,33 @@ namespace sprawdzianZadanie4
         {
             Console.WriteLine("Hello World!");
         }
+
+        public static void Print(int[] a, int[] b)
+        {
+            int i = 0;
+            int j = 0;
+            string wynik = "";
+            for (; i < a.Length; i++)
+            {
+                for (; j < b.Length; j++)
+                {
+
+                }
+            }
+            Console.WriteLine(wynik);
+        }
+        static void Main()
+        {
+            int[] a = new int[] { 0, 1, 1, 2, 3, 3, 3 };
+            int[] b = new int[] { 0, 1, 2, 3, 3 };
+            //int[] a = new int[] { -2, -1, 0, 1, 4 };
+            //int[] b = new int[] { -3, -2, -1, 1, 2, 3 };
+
+            //int[] a = new int[] { -2, -1, 0, 1, 4 };
+            //int[] b = new int[] { -2, -1, 0, 1, 4, 5, 6 };
+            Print(a, b);
+        }
+
     }
+
 }
