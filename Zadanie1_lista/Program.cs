3898b144769408d8ce79cb540c07c44bca589af1
krystianpetek <krystianpetek2@gmail.com>
2021-05-29 08:47:13 +0200

    Initial commit

---
+++ Zadanie1_lista/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Zadanie1_lista
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ lab07/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace lab07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
