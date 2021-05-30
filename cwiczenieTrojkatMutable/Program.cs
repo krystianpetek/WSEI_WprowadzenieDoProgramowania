2b887fbde59ab86d61c7da86775d6ff4ccaa732d
krystianpetek <krystianpetek2@gmail.com>
2021-05-31 00:51:44 +0200

    cwiczenieTrojkat

--- cwiczenieTrojkatMutable/Program.cs
+++ cwiczenieTrojkatMutable/Program.cs
@@ -4,9 +4,102 @@ namespace cwiczenieTrojkatMutable
 {
     class Program
     {
+        static double SprawdzA()
+        {
+            double a;
+            Console.Write("Podaj dlugosc A: ");
+            if (double.TryParse(Console.ReadLine(), out a) == false)
+            {
+                throw new ArgumentOutOfRangeException("błąd");
+            }
+            else if (a < 0)
+                throw new ArgumentOutOfRangeException("błąd");
+            return a;
+        }
+        static double SprawdzB()
+        {
+            double b;
+            Console.Write("Podaj dlugosc B: ");
+            if (double.TryParse(Console.ReadLine(), out b) == false)
+            {
+                throw new ArgumentOutOfRangeException("błąd");
+            }
+            else if (b < 0)
+                throw new ArgumentOutOfRangeException("błąd");
+            return b;
+        }
+        static double SprawdzC()
+        {
+            double c;
+            Console.Write("Podaj dlugosc C: ");
+            if (double.TryParse(Console.ReadLine(), out c) == false)
+            {
+                throw new ArgumentOutOfRangeException("błąd");
+            }
+            else if (c < 0)
+                throw new ArgumentOutOfRangeException("błąd");
+            return c;
+        }
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double a, b, c;
+
+            a = SprawdzA();
+            b = SprawdzB();
+            c = SprawdzC();
+
+            //Warunek trójkąta
+            double[] tab = { a, b, c };
+            Array.Sort(tab);
+            a = tab[0];
+            b = tab[1];
+            c = tab[2];
+
+            if (c < a + b)
+            {
+                TrojkatM trojkat = new TrojkatM(a, b, c);
+
+                trojkat.ParametryTrojkata();
+
+                a = SprawdzA();
+                b = SprawdzB();
+                c = SprawdzC();
+                tab = new double[] { a, b, c };
+                Array.Sort(tab);
+                a = tab[0];
+                b = tab[1];
+                c = tab[2];
+
+                if (c < a + b)
+                {
+                    trojkat.ParametryTrojkata();
+
+                    Console.WriteLine("Podaj współczynnik skali: ");
+                    int skala = int.Parse(Console.ReadLine());
+                    try
+                    {
+                        trojkat.Skala(skala);
+                        trojkat.ParametryTrojkata();
+
+                    }catch(ArgumentException)
+                    {
+                        Console.WriteLine("błąd w przeskalowaniu");
+                    }
+                }
+                else
+                {
+                    throw new ArgumentException("błąd");
+                }
+
+            }
+            else
+            {
+                throw new ArgumentException("błąd");
+            }
+
+
         }
     }
 }
--- cwiczenieTrojkatMutable/TrojkatM.cs
+++ cwiczenieTrojkatMutable/TrojkatM.cs
@@ -8,5 +8,77 @@ namespace cwiczenieTrojkatMutable
 {
     class TrojkatM
     {
+        private double dlugoscA = 1;
+
+        public double A
+        {
+            get { return dlugoscA; }
+            set { dlugoscA = value; }
+        }
+
+        private double dlugoscB = 1;
+
+        public double B
+        {
+            get { return dlugoscB; }
+            set { dlugoscB = value; }
+        }
+
+        private double dlugoscC = 1;
+
+        public double C
+        {
+            get { return dlugoscC; }
+            set { dlugoscC = value; }
+        }
+
+
+        public TrojkatM(double a, double b, double c)
+        {
+            A = a;
+            B = b;
+            C = c;
+        }
+
+        public TrojkatM()
+        {
+            
+        }
+
+        public void ParametryTrojkata() // https://pl.numberempire.com/arbitrary_triangle_calculator.php
+        {
+            double obwod = A + B + C;
+            Console.WriteLine($"Obwód: {obwod}");
+            double p = (A + B + C) / 2;
+            double pole = Math.Sqrt(p * (p - A) * (p - B) * (p - C));
+            Console.WriteLine($"Pole: {pole}");
+
+            if (C * C == A * A + B * B) // c2 = a2 + b2  trójkąt jest prostokątny
+                Console.WriteLine("Trójkąt jest prostokątny");
+
+            else if (C * C > A * A + B * B) // c2 > a2 + b2  trójkąt jest rozwartokątny
+                Console.WriteLine("Trójkąt jest rozwartokątny");
+
+            else if (C * C < A * A + B * B) // c2 < a2 + b2  trójkąt jest ostrokątny
+                Console.WriteLine("Trójkąt jest ostrokątny");
+
+            if(A == B && B == C)
+                Console.WriteLine("Trójkąt jest równoboczny");
+            else if( A == B || B == C || C == A)
+                Console.WriteLine("Trójkąt jest równoramienny");
+        }
+        public void Skala(double skala)
+        {
+            if(skala > 0)
+            {
+                A *= skala;
+                B *= skala;
+                C *= skala;
+            }
+            else
+            {
+                throw new ArgumentException();
+            }
+        }
     }
 }
