6de07bb1d2b9cc03a78e9041ca8f980820ae8b3a
krystianpetek <krystianpetek2@gmail.com>
2021-05-30 19:44:28 +0200

    initial

---
+++ cwiczenieTrojkatMutable/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace cwiczenieTrojkatMutable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
