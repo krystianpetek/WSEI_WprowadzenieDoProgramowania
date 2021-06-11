251087bdf2c0ac41231a7240e911870e456a0bf1
krystianpetek <krystianpetek2@gmail.com>
2021-06-11 23:29:00 +0200

    zadanieUproszczonyStosGeneryczny

--- zadanieUproszczonyStosGeneryczny/Program.cs
+++ zadanieUproszczonyStosGeneryczny/Program.cs
@@ -1,12 +1,35 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace zadanieUproszczonyStosGeneryczny
+namespace StrukturaStos
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+
+            var stos = new Stos<char>();
+            if (stos is StrukturaStos.IStos<char>)
+                Console.WriteLine("Stos<T> implemented");
+            else
+                Console.WriteLine("Stos<T> not implemented");
+
+            var s = new Stos<string>();
+            s.Push("aaa");
+            s.Push("bbb");
+            s.Push("ccc");
+            foreach (var x in s.ToArray())
+            {
+                Console.WriteLine(x);
+            }
+            Console.WriteLine($"na szczycie jest: {s.Peek}");
+            Console.WriteLine($"liczba elementów na stosie: {s.Count}");
+            while (!s.IsEmpty)
+            {
+                Console.WriteLine($"zdejmuje: {s.Pop()}");
+            }
+            if (s.IsEmpty)
+                Console.WriteLine("stos jest pusty");
         }
     }
 }
