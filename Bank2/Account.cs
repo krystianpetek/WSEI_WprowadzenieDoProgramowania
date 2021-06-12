f6147c9419fe10c0d078e57121e7197e9af9d690
krystianpetek <krystianpetek2@gmail.com>
2021-06-13 01:07:04 +0200

    Bank2

---
+++ Bank2/Account.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+namespace Bank
+{
+    class Account : IAccount
+    {
+        public string Name
+        {
+            get;
+        }
+
+        public decimal Balance
+        {
+            get;
+            private set;
+        }
+
+        public bool IsBlocked
+        {
+            get; private set;
+        }
+
+
+        public void Block()
+        {
+            IsBlocked = true;
+        }
+
+        public bool Deposit(decimal amount)
+        {
+            if (amount < 0)
+                return false;
+            else if (IsBlocked)
+                return false;
+            else
+            {
+                Balance = decimal.Round(Balance, 4) + decimal.Round(amount, 4);
+                return true;
+            }
+        }
+        public void Unblock()
+        {
+            IsBlocked = false;
+        }
+
+        public bool Withdrawal(decimal amount)
+        {
+            if (amount <= 0)
+                return false;
+            else if (Balance < 0)
+                return false;
+            else if (IsBlocked)
+                return false;
+            else if (decimal.Round(amount, 4) > decimal.Round(Balance, 4))
+
+                return false;
+            else
+            {
+                Balance = decimal.Round(Balance, 4) - decimal.Round(amount, 4);
+                return true;
+            }
+        }
+
+        public Account(string nazwa, decimal amount = 0)
+        {
+            if (nazwa == null)
+                throw new ArgumentOutOfRangeException();
+
+            nazwa = nazwa.Trim();
+
+            if (nazwa.Length > 2)
+            {
+                Name = nazwa;
+            }
+            else
+                throw new ArgumentException();
+
+            if (amount >= 0)
+                Balance = decimal.Round(amount, 4);
+            else
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        public override string ToString()
+        {
+            if (IsBlocked)
+                return $"Account name: {Name}, balance: {decimal.Round(Balance, 2):F2}, blocked";
+            else
+                return $"Account name: {Name}, balance: {decimal.Round(Balance, 2):F2}";
+        }
+    }
+
+}
---
+++ Bank2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Bank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var accou1nt = new Account(null, 100.0m);
+                Console.WriteLine(accou1nt);
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                Console.WriteLine("Name is null");
+            }
+
+            var account = new Account("John");
+            account.Deposit(100.00m);
+            Console.WriteLine(account.Withdrawal(10.00m));
+            Console.WriteLine(account);
+            Console.WriteLine(account.Withdrawal(100.00m));
+            Console.WriteLine(account);
+            Console.WriteLine(account.Withdrawal(0.00m));
+            Console.WriteLine(account);
+            Console.WriteLine(account.Withdrawal(-10.00m));
+            Console.WriteLine(account);
+            account.Block();
+            Console.WriteLine(account.Withdrawal(10.4999m));
+            Console.WriteLine(account);
+        }
+    }
+}
