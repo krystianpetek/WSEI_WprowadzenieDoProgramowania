e8f0ae272d5c0818b6e54b985925cf3edd3bf0c8
krystianpetek <krystian.petek@microsoft.wsei.edu.pl>
2021-06-28 17:51:48 +0200

    update

---
+++ SprawdzianProbnyZadanie2/Player.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SprawdzianProbnyZadanie2
+{
+    public class Player
+    {
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { 
+                
+                if(value.Length > 3)
+                    name = value; 
+            
+                for(int i = 0;i<value.Length;i++)
+                {
+                    if()
+                }
+            }
+        }
+
+    }
+}
